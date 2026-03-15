3bc0f4b6698153585530e4a352cacd3e146b4dfb
Han Boetes <hboetes@gmail.com>
2026-03-15 20:02:36 +0100

    fgrep is deprecated and untabify

--- opt/mycode/find_unlinked.sh
+++ opt/mycode/find_unlinked.sh
@@ -3,12 +3,11 @@ rm -f pkg_list
 for i in $(find /usr{,/local}/{bin,lib} -type f); do
     echo -ne '\r'
     echo -n "$i "
-    # if file $i | grep -q 'ELF.*bit LSB shared object' 2>&1 > /dev/null; then
-    if file $i | fgrep -q 'bit LSB shared object' 2>&1 > /dev/null; then
-	if ! ldd $i > /dev/null 2>&1; then
-	    echo "appears to be unlinked" >&2
+    if file $i | grep -Fq 'bit LSB shared object' 2>&1 > /dev/null; then
+        if ! ldd $i > /dev/null 2>&1; then
+            echo "appears to be unlinked" >&2
             pkg_locate $i| cut -d: -f1 >> pkg_list
-	fi
+        fi
     fi
 done
 echo These packages need updating:
